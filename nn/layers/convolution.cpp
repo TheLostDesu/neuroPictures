1ed632c46aee58bc7c0cff4ee9357c1b6cabd462
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2023-06-07 21:09:33 +0300

    Update convolution.cpp
    3)В сверточном слое добавлены функции нахождения ошибки, нахождения скрытой ошибки, нахождения ошибки

--- nn/layers/convolution.cpp
+++ nn/layers/convolution.cpp
@@ -12,3 +12,29 @@ class convolutionLayer
     private:
         std::vector<matrix> convolutionMatrixes;
 };
+
+double calculate_error(double output_result, double result){
+    return (result-output_result)*(result-output_result)
+}
+
+void update_weights(vector<double>& link_weights, vector<double>& delta, double learning_rate, vector<double>& activations){
+    for (size_t i = 0; i < link_weights.size(); ++i){
+        for (size_t j = 0; j < link_weights[0].size(); ++j){
+            weight[i][j] += learning_rate * delta[i] * activations[i];
+        }
+    }
+}
+
+vector<double> calculate_hidden_error(vector<double>& delta, vector<double>>& link_weights) {
+    vector<double> hidden_error(link_weights[0].size(), 0.0);
+
+    for (int i = 0; i < link_weights[0].size(); ++i) {
+        double error = 0.0;
+        for (int j = 0; j < link_weights.size(); ++j) {
+            sum += delta[j] * link_weights[j][i];
+        }
+        hidden_error[i] = error;
+    }
+
+    return hidden_error;
+}
