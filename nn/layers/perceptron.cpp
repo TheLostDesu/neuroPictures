32fd5fa9c78a39c6c262ff21b78fcd787e28ec38
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2023-06-07 20:36:39 +0300

    Update perceptron.cpp
    2) В перцептропном слое рассчитывается скрытая ошибка

--- nn/layers/perceptron.cpp
+++ nn/layers/perceptron.cpp
@@ -37,3 +37,22 @@ void update_weights(vector<double>& input, vector<double>& gradients, double lea
         }
     }
 }
+
+vector<double> calculate_hidden_error(vector<double>& gradients,Matrix& next_layer_weights) {
+    int input_size = weights.size();
+    int output_size = gradients.size();
+
+    vector<double> hidden_error(input_size);
+
+    for (int i = 0; i < input_size; ++i) {
+        double error = 0.0;
+        for (int j = 0; j < output_size; ++j) {
+            error += gradients[j] * next_layer_weights[i][j];
+        }
+
+        hidden_error[i] = error;
+    }
+
+    return hidden_error;
+}
+};
