621678b6386e02195d72757ac3e7b85c30dbe98e
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2023-06-12 15:00:13 +0300

    преобразование изображений в матрицы цветов
    преобразование изображений в матрицы цветов

--- nn/utility.cpp
+++ nn/utility.cpp
@@ -59,55 +59,14 @@ int max(int a, int b, int c, int d)
     return std::max(std::max(a, b), std::max(c, d)); 
 }
 <<<<<<< HEAD
-//Для красного цвета
-std::vector<int> red PixelsToVector(std::string& imagePath) {
+std::vector<cv::Mat> getRGBMatrices(std::string& imagePath) {
     cv::Mat image = cv::imread(imagePath);
 
-    std::vector<int> pixelVector;
+    std::vector<cv::Mat> rgbMatrices;
+    cv::split(image, rgbMatrices);
 
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-            int redValue = pixel[2];
-            pixelVector.push_back(redValue);
-        }
-    }
-
-    return pixelVector;
+    return rgbMatrices;
 }
-//Для синего цвета
-std::vector<int> bluePixelsToVector(std::string& imagePath) {
-    cv::Mat image = cv::imread(imagePath);
-
-    std::vector<int> pixelVector;
-
-
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-            int blueValue = pixel[2]; // Значение синего канала (BGR формат)
-            pixelVector.push_back(blueValue);
-        }
-    }
-
-    return pixelVector;
-}
-//Для зеленого цвета
-std::vector<int> greenPixelsToVector(std::string& imagePath) {
-    cv::Mat image = cv::imread(imagePath);
-
-    std::vector<int> pixelVector;
-
-
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-            int blueValue = pixel[1]; // Значение синего канала (BGR формат)
-            pixelVector.push_back(blueValue);
-        }
-    }
-
-    return pixelVector;
 =======
 
 
