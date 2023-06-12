cd90ef5f079c1412240b84d841fcddac6b985218
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2023-06-12 14:46:21 +0300

    преобразование изображений в матрицы
    Преобразование изображений в матрицы цветов красный, синий и зеленый

--- nn/utility.cpp
+++ nn/utility.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <opencv2/opencv.hpp>
 
 class matrix
 {
@@ -52,4 +53,54 @@ class matrix
 int max(int a, int b, int c, int d) 
 {
     return std::max(std::max(a, b), std::max(c, d)); 
+}
+//Для красного цвета
+std::vector<int> red PixelsToVector(std::string& imagePath) {
+    cv::Mat image = cv::imread(imagePath);
+
+    std::vector<int> pixelVector;
+
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+            int redValue = pixel[2];
+            pixelVector.push_back(redValue);
+        }
+    }
+
+    return pixelVector;
+}
+//Для синего цвета
+std::vector<int> bluePixelsToVector(std::string& imagePath) {
+    cv::Mat image = cv::imread(imagePath);
+
+    std::vector<int> pixelVector;
+
+
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+            int blueValue = pixel[2]; // Значение синего канала (BGR формат)
+            pixelVector.push_back(blueValue);
+        }
+    }
+
+    return pixelVector;
+}
+//Для зеленого цвета
+std::vector<int> greenPixelsToVector(std::string& imagePath) {
+    cv::Mat image = cv::imread(imagePath);
+
+    std::vector<int> pixelVector;
+
+
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+            int blueValue = pixel[1]; // Значение синего канала (BGR формат)
+            pixelVector.push_back(blueValue);
+        }
+    }
+
+    return pixelVector;
 }
